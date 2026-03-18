fddd3ea76f9372fde4ba0b92e026e25d1ebbb02f
GuYue <1535389126@qq.com>
2026-03-18 16:22:15 +0800

    54 - 8

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -27,6 +27,7 @@ public:
 	
 	
 	void AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupAbilities);
+	void AddCharacterPassiveAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupPassiveAbilities);
 	
 	bool bStartupAbilitiesGiven = false;
 	
